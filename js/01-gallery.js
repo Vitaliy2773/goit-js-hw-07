c8ee5a4ff6e855aa4a5c0d9b26b70b8199846c6e
Vitaliy2773 <vitalii64773@gmail.com>
2023-08-17 15:59:03 -1100

    task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+
+
+const galleryList = document.querySelector(".gallery")
+
+
+const renderGallery = (arr) => { 
+    return arr.map(item => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`).join("")
+
+}
+
+const gallery = renderGallery(galleryItems)
+
+galleryList.innerHTML = gallery
+
+
+const selectedImg = (e) => {
+    e.preventDefault()
+    const { target } = e
+    if (target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+   <img src="${target.dataset.source}" width="800" height="600">
+  `);
+    instance.show()
+
+
+
+    galleryList.addEventListener("keydown", (e) => {
+        if (e.code === "Escape") {
+            instance.close();
+        }
+    })
+}
+
+    galleryList.addEventListener("click", selectedImg)
+
