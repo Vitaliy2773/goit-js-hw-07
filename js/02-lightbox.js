e83abb55cd6076e670bb95a57fad952f85c50243
Vitaliy2773 <vitalii64773@gmail.com>
2023-08-17 18:26:04 -1100

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector(".gallery")
+
+
+const renderGallery = (arr) => { 
+    return arr.map(item => `<li  class="gallery__item ">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image "
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`).join("")
+
+}
+
+const gallery = renderGallery(galleryItems)
+
+galleryList.innerHTML = gallery
+
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt",captionDelay: 250});
